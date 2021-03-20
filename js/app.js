2ec9c81c9a2f92048e7556aae9c32a9b6103b74c
Kid-AAA <46485121+Kid-AAA@users.noreply.github.com>
2021-03-20 16:22:08 +0000

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/WebGL.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/MarkerlessAR.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
